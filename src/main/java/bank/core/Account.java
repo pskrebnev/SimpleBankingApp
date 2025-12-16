63bc3faaa39e6d4203abf729e7b90dc32291bcc1
Pavel Skrabneu <skrebnev@gmail.com>
2025-12-16 16:28:35 +0100

    validation changed in 'Account'

--- src/main/java/bank/core/Account.java
+++ src/main/java/bank/core/Account.java
@@ -75,6 +75,8 @@ public class Account {
     balance = balance.subtract(amount);
   }
 
+  
+
   // validate amount format and value
   private void validateAmount(BigDecimal amount) throws BankingSystemException {
     if (amount == null) {
@@ -92,14 +94,26 @@ public class Account {
 
   @Override
   public boolean equals(Object o) {
-    if (this==o) return true;
-    if (o==null || getClass() != o.getClass()) return false;
+    if (this == o) {
+      return true;
+    }
+    if (o == null || getClass() != o.getClass()) {
+      return false;
+    }
 
     Account account = (Account) o;
     return Objects.equals(accountNumber, account.accountNumber);
   }
 
+  @Override
+  public int hashCode() {
+    return Objects.hash(accountNumber);
+  }
 
-
-
+  @Override
+  public String toString() {
+    String ownerInfo = isSourceAccount ? "SOURCE" : owner.getName();
+    return String.format("Account{number='%s', balance=$%.2f, owner=%s}",
+        accountNumber, balance, ownerInfo);
+  }
 }
